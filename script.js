6652f420bc168a1371078a17ad358d20dd18280f
mchicha <mordehaich@gmail.com>
2018-04-26 11:25:12 +0200

    Partial Solution of the challenge

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 var RecipeApp = function () {
 
     var recipes = [
-        // { 
-        //     name: 'Best Chicken Soup!', 
-        //     image: 'https://static01.nyt.com/images/2016/11/29/dining/recipelab-chick-noodle-still/recipelab-chick-noodle-still-master675.jpg',
-        //     ingredients: [
-        //         { name: 'whole chicken' },
-        //         { name: 'medium carrots'},
-        //         { name: 'onions' },
-        //     ] 
-        // }
+        { 
+            name: 'Best Chicken Soup!', 
+            image: 'https://static01.nyt.com/images/2016/11/29/dining/recipelab-chick-noodle-still/recipelab-chick-noodle-still-master675.jpg',
+            ingredients: [
+                { name: 'whole chicken' },
+                { name: 'medium carrots'},
+                { name: 'onions' },
+            ] 
+        }
     ];
 
-    var $recipes = $('.recipes');
 
     //id's for recipes
     var recId = 0;
@@ -32,17 +31,33 @@ var RecipeApp = function () {
         recId ++; 
 
         recipes.push(recipe);
-    };
+    }; 
+    function addIngredient(text,recipeId){
 
-    var createIngredients = function(){
-        //add code
+        var recipe = _findPostById(id);
+        var ingredient = { text: textIngredient };
+        recipe.ingredients.push(ingredient);
+        
+    }
+
+    var createIngredients = function(currentRecipe, textIngredient){
+        //get data from the view
+        var $clickedRecipe = $(currentRecipe).closest('.recipe');
+        var id = $clickedRecipe.data().id;
+
+        addIngredient(textIngredient,id)
     };
 
     var _getIngredients = function(recipe){
-        var recipesHTML = "";
-
-        //add code
-        return recipesHTML;
+        var liIngredients = "";
+        if (recipe.ingredients.length > 0)
+        {
+            // 2 - Loop through the ingredients array,
+            for (var i=0; i < recipe.ingredients.length; i++) {
+                liIngredients+= "<li>" + recipe.ingredients[i].name + "</li>";
+            }
+        }
+        return liIngredients;
     };
 
     var renderRecipes = function () {
@@ -54,20 +69,20 @@ var RecipeApp = function () {
             var recipe = recipes[i];
 
             //return HTML for all ingredients
-            var ingredients = _getIngredients(); //add code
-
+            var ingredients = _getIngredients(recipe); //add code
+            debugger;
             $recipes.append(
-                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' + 
+                '<div class="   col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' + 
                     '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
                     '<img class="recipe-img" src="' + recipe.image + '"/>' +
                     '<hr>' +
                     '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
                     '<div class="input-group mb-3">' +
                         '<div class="input-group-prepend">' +
-                            '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
+                        '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
+                        '<input type="text" class="ingredient-name">' +
                         '</div>' + 
-                        '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
-                        
+                        '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +   
                     '</div>' +
                     '<ul class="ingredients">' + ingredients + '</ul>'+
                 '</div>'
@@ -78,9 +93,10 @@ var RecipeApp = function () {
     return {
         createRecipe: createRecipe,
         renderRecipes: renderRecipes,
-        // createIngredients: createIngredients,
+        createIngredients: createIngredients
     }
 };
+var $recipes = $('.recipes');
 
 var app = RecipeApp();
 
@@ -96,5 +112,15 @@ $('.add-recipe').on('click', function(){
     //add recipe to array and render
     app.createRecipe(name, image);
     app.renderRecipes();
+});
+
+
+$recipes.on('click','.add-ingredients', function () {
+    // Get the input comment of the current post we enter
+    var text = $(this).closest(".recipe").find('.ingredient-name').val();
+
+    app.createIngredients(this, text);
+    // Update the view of the comments
+    app.renderRecipes(this);
 });
 
